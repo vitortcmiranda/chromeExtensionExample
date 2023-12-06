bec89639c7f21f3c8fd55475ddf292415142a131
vitor.miranda <vitortcmout@gmail.com>
2023-12-06 10:50:14 -0300

    removing elements

---
+++ content.js
@@ -0,0 +1,6 @@
+var elementToRemove = document.querySelector('.cleitao');
+var anotherElementToRemove = document.querySelector('.ytd-enforcement-message-view-model.style-scope') 
+
+if (elementToRemove  || anotherElementToRemove) {
+  elementToRemove.remove();
+}
